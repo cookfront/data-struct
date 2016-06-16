f71828a6bc78eaad2424b61175a9120446c6893c
cookfront <cookfront@192.168.0.102>
2016-06-16 21:15:07 +0800

    update insert sort

--- sort/insert-sort/insertSort.js
+++ sort/insert-sort/insertSort.js
@@ -1,17 +1,14 @@
 function insertSort(arr) {
 	var len = arr.length;
-	var tmp;
+
 	for (var i = 1; i < len; i++) {
-		tmp = arr[i];
-		for (var j = i; j > 0; j--) {
-			if (tmp < arr[j - 1]) {
-				arr[j] = arr[j - 1];
-			} else {
-				break;
-			}
+		var item = arr[i];
+		for (var j = i - 1; j >= 0 && arr[j] > item; j--) {
+			arr[j + 1] = arr[j];
 		}
-		arr[j] = tmp;
+		arr[j + 1] = item;
 	}
+
 	return arr;
 }
 
